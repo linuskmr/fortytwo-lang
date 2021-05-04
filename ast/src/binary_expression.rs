a7eaf3899bb3586c4ebffd291b07988cd1bc42e0
linuskmr <56581587+linuskmr@users.noreply.github.com>
2021-05-04 19:58:21 +0200

    Started with binary expressions

--- ast/src/binary_expression.rs
+++ ast/src/binary_expression.rs
@@ -1,13 +1,13 @@
 use crate::AST;
 
 #[derive(Debug)]
-pub struct BinaryOperation<L: AST, R: AST> {
+pub struct BinaryExpression<L: AST, R: AST> {
     lhs: L,
     operation: char,
     rhs: R,
 }
 
-impl<L: AST, R: AST> AST for BinaryOperation<L, R> {
+impl<L: AST, R: AST> AST for BinaryExpression<L, R> {
     fn pretty(&self) -> String {
         format!("{} {} {}", self.lhs.pretty(), self.operation, self.rhs.pretty())
     }
--- ast/src/lib.rs
+++ ast/src/lib.rs
@@ -1,6 +1,6 @@
 use std::fmt::Debug;
 
-pub use binary_operation::BinaryOperation;
+pub use binary_expression::BinaryExpression;
 pub use function::{Function, FunctionPrototype};
 pub use function_call::FunctionCall;
 pub use number::Number;
@@ -8,7 +8,7 @@ pub use variable::Variable;
 
 mod number;
 mod variable;
-mod binary_operation;
+mod binary_expression;
 mod function_call;
 mod function;
 
