ca1bc85a93577a33d9572a73058606c597254da8
linuskmr <56581587+linuskmr@users.noreply.github.com>
2021-10-12 10:40:25 +0200

    Add unwrapping of tokens and always show help if args are present

--- src/bin/ftlld.rs
+++ src/bin/ftlld.rs
@@ -7,7 +7,7 @@ use fortytwo_lang::position_reader::PositionReader;
 fn main() -> miette::Result<()> {
     let args: Vec<_> = env::args().collect();
     match args.get(1) {
-        Some(arg) if arg == "--help" || arg == "-h" => show_help(),
+        Some(_) => show_help(),
         _ => lexer_from_stdin()?,
     }
     Ok(())
@@ -18,11 +18,8 @@ fn lexer_from_stdin() -> miette::Result<()> {
     stdin().read_to_string(&mut sourcecode).expect("Could not read sourcecode from stdin");
     let named_source = NamedSource::new("stdin", sourcecode.clone());
     let lexer = Lexer::new(PositionReader::new(sourcecode.chars()), named_source);
-    for symbol in lexer {
-        if let Err(e) = symbol {
-            return Err(e.into())
-        }
-        println!("{:?}", symbol)
+    for token in lexer {
+        println!("{:?}", token?);
     }
     Ok(())
 }
@@ -31,10 +28,8 @@ fn show_help() {
     println!(
         r#"FORTYTWO-LANG LEXER DUMP
 Dumps the output of the lexer.
-Write ftl sourcecode to stdin and end stdin by pressing CTRL+C.
+Write your ftl sourcecode to stdin and end stdin by pressing CTRL+C.
 
 USAGE:
-    ftlld
-"#
-    )
+    ftlld"#)
 }
