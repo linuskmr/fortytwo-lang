94e8ce31cceea0b4a00bddd1c708f90768190f61
linuskmr <56581587+linuskmr@users.noreply.github.com>
2023-07-08 13:49:58 +0200

    Add offset to position

--- src/source/position.rs
+++ src/source/position.rs
@@ -1,43 +1,53 @@
+use crate::source::Source;
 use std::fmt;
 use std::ops::RangeInclusive;
 use std::sync::Arc;
-use crate::source::Source;
-
 
 /// Line and column in source code.
-#[derive(PartialEq, Eq, Hash, Clone, Debug, Copy, Default)]
+#[derive(PartialEq, Eq, Hash, Clone, Debug, Copy)]
 pub struct Position {
-	pub line: usize,
-	pub column: usize,
+    pub line: usize,
+    pub column: usize,
+    pub offset: usize,
 }
 
 impl Position {
-	pub fn user_line(&self) -> usize {
-		self.line + 1
-	}
+    pub fn user_line(&self) -> usize {
+        self.line
+    }
 
-	pub fn user_column(&self) -> usize {
-		self.column + 1
-	}
+    pub fn user_column(&self) -> usize {
+        self.column
+    }
 }
 
 impl fmt::Display for Position {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(f, "{}:{}", self.user_line(), self.user_column())
-	}
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:{}", self.user_line(), self.user_column())
+    }
 }
 
+impl Default for Position {
+    fn default() -> Self {
+        Position {
+            line: 1,
+            column: 1,
+            offset: 0,
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn test_display() {
-		let position = Position {
-			line: 42,
-			column: 5,
-		};
-		assert_eq!(position.to_string(), "43:6")
-	}
+    #[test]
+    fn test_display() {
+        let position = Position {
+            line: 42,
+            column: 5,
+            offset: 1337,
+        };
+        assert_eq!(position.to_string(), "42:5")
+    }
 }
--- src/source/source_position.rs
+++ src/source/source_position.rs
@@ -1,53 +1,51 @@
-use std::fmt;
-use std::sync::Arc;
 use crate::source::position_range::PositionRange;
 use crate::source::Source;
+use std::fmt;
+use std::sync::Arc;
 
 /// Position in the source code ranging from start to end (both inclusive).
 #[derive(PartialEq, Eq, Hash, Clone, Debug)]
 pub struct SourcePositionRange {
-	pub source: Arc<Source>,
-	pub position: PositionRange,
+    pub source: Arc<Source>,
+    pub position: PositionRange,
 }
 
 impl SourcePositionRange {
-	pub fn get_affected_lines(&self) -> Vec<String> {
-		self.source.text[self.position.start.line..=self.position.end.line]
-			.iter()
-			.map(|line| line.iter().collect())
-			.collect()
-	}
+    pub fn get_affected_code(&self) -> String {
+        self.source.text[self.position.start.offset..=self.position.end.offset]
+            .iter()
+            .collect::<String>()
+    }
 }
 
 impl fmt::Display for SourcePositionRange {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(f, "{}:{}", self.source.name, self.position.start)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:{}", self.source.name, self.position.start)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use crate::source::Position;
-	use super::*;
+    use super::*;
+    use crate::source::Position;
 
-	#[test]
-	fn test_display() {
-		let position = SourcePositionRange {
-			source: Arc::new(Source::new(
-				"file.name".to_owned(),
-				"text...".to_owned(),
-			)),
-			position: PositionRange {
-				start: Position {
-					line: 42,
-					column: 5,
-				},
-				end: Position {
-					line: 43,
-					column: 1,
-				}
-			}
-		};
-		assert_eq!(position.to_string(), "file.name:43:6")
-	}
+    #[test]
+    fn test_display() {
+        let position = SourcePositionRange {
+            source: Arc::new(Source::new("file.name".to_owned(), "text...".to_owned())),
+            position: PositionRange {
+                start: Position {
+                    line: 42,
+                    column: 5,
+                    offset: 1337,
+                },
+                end: Position {
+                    line: 43,
+                    column: 1,
+                    offset: 1340,
+                },
+            },
+        };
+        assert_eq!(position.to_string(), "file.name:42:5")
+    }
 }
