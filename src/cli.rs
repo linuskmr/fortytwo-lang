54bb4b5c51a4165b5c2a0900641fa2bf6910b392
linuskmr <linuskmr.dev@gmail.com>
2024-12-23 16:23:16 +0100

    Update dependency versions

--- src/cli.rs
+++ src/cli.rs
@@ -11,21 +11,18 @@ pub enum Command {
 	/// Format the code.
 	Fmt {
 		/// The file to format. Note that this file will be overwritten.
-		#[clap(parse(from_os_str))]
 		file: std::path::PathBuf,
 	},
 
 	/// Compile to an executable.
 	Compile {
 		/// The file to compile.
-		#[clap(parse(from_os_str))]
 		file: std::path::PathBuf,
 	},
 
 	/// Compile and execute.
 	Run {
 		/// The file to run.
-		#[clap(parse(from_os_str))]
 		file: std::path::PathBuf,
 	},
 }
